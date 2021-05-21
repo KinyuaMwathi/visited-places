e8e66c027834470fa3dead3e55b76b35f9200d81
Charles Mwathi <mwathi.kinyua@gmail.com>
2021-05-21 07:22:03 +0300

    Add a constructor for the destination information.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// Business Logic
+
+function destinationInformation (location, landmarks, visitedYear, notes) {
+  this.location = location;
+  this.landmarks = landmarks;
+  this.visitedYear = visitedYear;
+  this.notes = notes;
+}
