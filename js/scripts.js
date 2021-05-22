6d21b8c038f4acbdc4d265bd0ff0ee946c559871
Charles Mwathi <mwathi.kinyua@gmail.com>
2021-05-22 07:35:12 +0300

    Add a destination collection constructor.

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,18 @@ function destinationInformation (location, landmarks, visitedYear, notes) {
   this.visitedYear = visitedYear;
   this.notes = notes;
 }
+
+function DestinationCollection() {
+  this.destinations = {};
+  this.currentId = 0;
+}
+
+DestinationCollection.prototype.addDestination = function(destination) {
+  destination.id = this.assignId();
+  this.destinations[destination.id] = destination;
+}
+
+DestinationCollection.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
